30a8a61c7a5babe6cd6462d611bbcace45c5b312
İhsan Eser <eser2679@gmail.com>
2021-10-19 00:34:52 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("merhaba kodlama.io")
+
+
+let dolarDun = 9.20
+
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//EuroDun = 11
+
+let konutKredileri = ["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+
